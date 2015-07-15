87526643e6a2195a84366fe4ca2f97c8702115fe
undertuga <undertuga@tutanota.de>
2015-07-15 11:59:34 +0100

    1st commit...

---
+++ amqConfig.php
@@ -0,0 +1,12 @@
+<?php
+
+/*
+	Connection related arguments
+	-- change according to your needs
+
+	@author undertuga <undertuga@tutanota.de>
+*/
+define('host', '127.0.0.1');
+define('port', 5672);
+define('user', 'demo');
+define('pass', 'demo');
